da218ae3c78a87ec4f006923838815b5bf2ce9dd
adatzer <ada.tzereme@gmail.com>
2019-05-18 12:01:47 +0000

    hardcode font of choice

--- config.h
+++ config.h
@@ -11,8 +11,8 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 /* ada defaulttransparency patch: next 1 line */
 static const double defaultopacity  = 0.75;
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "Biolinum:size=10" };
+static const char dmenufont[]       = "Biolinum:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
