5f99467f6d3d332df1f8daa936454c30e9a7b647
adatzer <ada.tzereme@gmail.com>
2020-02-29 13:07:06 +0000

    plus xinerama libs

--- config.mk
+++ config.mk
@@ -10,10 +10,10 @@ MANPREFIX = ${PREFIX}/share/man
 X11INC = /usr/X11R6/include
 X11LIB = /usr/X11R6/lib
 
-# ada Mar2019
+# ada
 # Xinerama, comment if you don't want it
-#XINERAMALIBS  = -lXinerama
-#XINERAMAFLAGS = -DXINERAMA
+XINERAMALIBS  = -lXinerama
+XINERAMAFLAGS = -DXINERAMA
 
 # freetype
 FREETYPELIBS = -lfontconfig -lXft
